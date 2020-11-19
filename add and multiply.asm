527a6ca45fc0b5faf14be3a95fe11368ef267e91
Nicolas <32076027+installgento0@users.noreply.github.com>
2020-11-19 13:07:51 -0300

    Update add and multiply.asm

--- add and multiply.asm
+++ add and multiply.asm
@@ -8,13 +8,19 @@
 # addi $t0, $t0, 1   (or any number up to 2^31 = 2147483648)
 # Multiply 5 by 4:
 main:
-	addi $a0, $zero, 5 # a0 = 5
+	addi $a0, $zero, 0 # a0 = 5
 	addi $a1, $zero, 4 # a1 = 4 --> count
 	addi $a2, $a2, 1
+	# check if one of the operands is zero
+	beq $a0,$zero, retzero
+	beq $a1,$zero, retzero	
 mult:
+	# check if the operation has ended
 	beq $a2, $a1, ret
 	addi $a0, $a0, 5 
 	addi $a2, $a2, 1
 	j mult
 ret:
+	add $v0, $v0, $a0 # v0 receives the result
+retzero:
 	add $v0, $v0, $a0 # v0 receives the result
